8d8a8adbb5dab449c047c5be74407d4f25a8a671
Pedro Alvarez <palvarez89@gmail.com>
2018-08-19 14:43:37 +0200

    Initial connection to different servers

--- gitlabirced/cli.py
+++ gitlabirced/cli.py
@@ -1,18 +1,150 @@
 # -*- coding: utf-8 -*-
 
 """Console script for gitlabirced."""
+import signal
 import sys
 import click
 
+import irc
+import yaml
+
 
 @click.command()
-def main(args=None):
+@click.argument('config-file', nargs=1)
+def main(config_file):
     """Console script for gitlabirced."""
-    click.echo("Replace this message by putting your code into "
-               "gitlabirced.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    click.echo(config_file)
+
+    try:
+        with open(config_file, 'r') as stream:
+            config = yaml.load(stream)
+            print("Configuration loaded %s" % config)
+    except yaml.YAMLError as exc:
+        print(exc)
+    except IOError:
+        print("File %s not found" % config_file)
+
+    bots = []
+    all_bots = connect_networks(config['networks'])
+
+    def signal_handler(sig, frame):
+        print('You pressed Ctrl+C!')
+        for b in all_bots:
+            all_bots[b]['process'].terminate()
+        click.Abort()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+
+    print('Press Ctrl+C')
+
     return 0
 
 
+from multiprocessing import Process
+
+def connect_networks(networks):
+    """ Connects to all the networks configured in the config file.
+
+    Returns a dictionary using the same keys as in the config file
+    containing process and bot object.
+    """
+
+    print("conf taken %s " % networks)
+    result = {}
+    all_bots = []
+    all_threads = []
+    for net in networks:
+        server = networks[net]['url']
+        port = networks[net]['port']
+        nick = networks[net]['nick']
+        channel = '##ironfoot'
+
+        bot = TestBot(channel, nick, server, net, port)
+        print("Starting %s" % server)
+        thread = Process(target=bot.start)
+        thread.start()
+
+        result[net] = {
+            'process': thread,
+            'bot': bot
+        }
+
+    return result
+
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
+
+
+import irc.bot
+import irc.strings
+from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
+
+
+class TestBot(irc.bot.SingleServerIRCBot):
+    def __init__(self, channel, nickname, server, net_name, port=6667):
+        irc.bot.SingleServerIRCBot.__init__(
+            self, [(server, port)], nickname, nickname)
+        self.channel = channel
+        self.net_name = net_name
+
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_")
+
+    def on_welcome(self, c, e):
+        c.join(self.channel)
+
+    def on_privmsg(self, c, e):
+        self.do_command(e, e.arguments[0])
+
+    def on_pubmsg(self, c, e):
+        print('on pubmsg')
+        print('received via', self.net_name)
+        print(c, e)
+        a = e.arguments[0].split(":", 1)
+        if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(
+                self.connection.get_nickname()):
+            self.do_command(e, a[1].strip())
+        return
+
+    def on_dccmsg(self, c, e):
+        # non-chat DCC messages are raw bytes; decode as text
+        text = e.arguments[0].decode('utf-8')
+        c.privmsg("You said: " + text)
+
+    def on_dccchat(self, c, e):
+        if len(e.arguments) != 2:
+            return
+        args = e.arguments[1].split()
+        if len(args) == 4:
+            try:
+                address = ip_numstr_to_quad(args[2])
+                port = int(args[3])
+            except ValueError:
+                return
+            self.dcc_connect(address, port)
+
+    def do_command(self, e, cmd):
+        nick = e.source.nick
+        c = self.connection
+
+        if cmd == "disconnect":
+            self.disconnect()
+        elif cmd == "die":
+            self.die()
+        elif cmd == "stats":
+            for chname, chobj in self.channels.items():
+                c.notice(nick, "--- Channel statistics ---")
+                c.notice(nick, "Channel: " + chname)
+                users = sorted(chobj.users())
+                c.notice(nick, "Users: " + ", ".join(users))
+                opers = sorted(chobj.opers())
+                c.notice(nick, "Opers: " + ", ".join(opers))
+                voiced = sorted(chobj.voiced())
+                c.notice(nick, "Voiced: " + ", ".join(voiced))
+        elif cmd == "dcc":
+            dcc = self.dcc_listen()
+            c.ctcp("DCC", nick, "CHAT chat %s %d" % (
+                ip_quad_to_numstr(dcc.localaddress),
+                dcc.localport))
+        else:
+            c.notice(nick, "Not understood: " + cmd)
