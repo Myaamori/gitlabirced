aae60ae29ab6cf3997b80586897be726bc47f1d4
Pedro Alvarez <palvarez89@gmail.com>
2018-08-20 01:53:03 +0200

    Some sort of  http server listening for webhooks

--- gitlabirced/cli.py
+++ gitlabirced/cli.py
@@ -5,9 +5,14 @@ import signal
 import sys
 import click
 
+from http.server import HTTPServer
+from http.server import BaseHTTPRequestHandler
+import json
+
 import irc
 import yaml
 
+import logging
 
 @click.command()
 @click.argument('config-file', nargs=1)
@@ -27,6 +32,18 @@ def main(config_file):
     bots = []
     all_bots = connect_networks(config['networks'])
 
+    hooks = parse_hooks(config['hooks'])
+    token = config['token']
+
+    def run_server(addr, port):
+        """Start a HTTPServer which waits for requests."""
+        httpd = MyHTTPServer(token, hooks, all_bots, (addr, port), RequestHandler)
+        httpd.serve_forever()
+        print('serving')
+
+    print('going to execute server')
+    run_server('0.0.0.0', 1337)
+
     def signal_handler(sig, frame):
         print('You pressed Ctrl+C!')
         for b in all_bots:
@@ -39,6 +56,11 @@ def main(config_file):
 
     return 0
 
+
+def parse_hooks(hooks):
+    print('parsing hooks')
+    print(hooks)
+    return hooks
 
 from multiprocessing import Process
 
@@ -82,6 +104,7 @@ from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
 
 class TestBot(irc.bot.SingleServerIRCBot):
     def __init__(self, channel, nickname, server, net_name, port=6667):
+        # TODO pass here the watchers configuration
         irc.bot.SingleServerIRCBot.__init__(
             self, [(server, port)], nickname, nickname)
         self.channel = channel
@@ -100,6 +123,7 @@ class TestBot(irc.bot.SingleServerIRCBot):
         print('on pubmsg')
         print('received via', self.net_name)
         print(c, e)
+        # TODO: detect the channel, check watchers configuration, and act on consequence
         a = e.arguments[0].split(":", 1)
         if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(
                 self.connection.get_nickname()):
@@ -148,3 +172,82 @@ class TestBot(irc.bot.SingleServerIRCBot):
                 dcc.localport))
         else:
             c.notice(nick, "Not understood: " + cmd)
+
+
+class MyHTTPServer(HTTPServer):
+
+    def __init__(self, token, hooks, bots, *args, **kw):
+        HTTPServer.__init__(self, *args, **kw)
+        self.token = token
+        self.hooks = hooks
+        self.bots = bots
+
+class RequestHandler(BaseHTTPRequestHandler):
+    """A POST request handler."""
+
+    def do_POST(self):
+        hooks = self.server.hooks
+        token = self.server.token
+        bots = self.server.bots
+        print(hooks)
+        logging.info("Hook received")
+
+        # get payload
+        header_length = int(self.headers.get('content-length', "0"))
+        json_payload = self.rfile.read(header_length)
+        print(json_payload)
+        json_params = {}
+        if len(json_payload) > 0:
+            json_params = json.loads(json_payload)
+
+        # get gitlab secret token
+        gitlab_token_header = self.headers.get('X-Gitlab-Token')
+
+        # get project homepage
+        project = json_params['project']['homepage']
+
+        try:
+            # get command and token from config file
+            gitlab_token = str(token)
+
+            logging.info("Load project '%s' and run command '%s'", project)
+        except KeyError as err:
+            self.send_response(500, "KeyError")
+            logging.error("Project '%s' not found in %s", project, args.cfg)
+            self.end_headers()
+            return
+
+        # Check if the gitlab token is valid
+        print(gitlab_token_header, gitlab_token)
+        if gitlab_token_header == gitlab_token:
+            logging.info('TOKEN VALID')
+            self._handle_push(json_params)
+            self.send_response(200, "OK")
+        else:
+            logging.error("Not authorized, Gitlab_Token not authorized")
+            self.send_response(401, "Gitlab Token not authorized")
+        self.end_headers()
+    
+    def _handle_push(self, json_params):
+        hooks = self.server.hooks
+        token = self.server.token
+        bots = self.server.bots
+        project = json_params['project']['path_with_namespace']
+        for h in hooks:
+            if h['project'] == project:
+                print('project found!!')
+                network = h['network']
+                reports = h['reports']
+                bot = bots[network]['bot']
+                for r in reports:
+                    if 'push' in reports[r]:
+                        print('sending to %s, in network %s' % (r, network))
+                        #bot.privmsg(r, 'pushhhh')
+                        bot.connection.privmsg("##ironfoot", "pushh")
+
+            else:
+                print("not found", project)
+        
+
+
+
