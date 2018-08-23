9bf951c9333151424c79c6e67751cd5ce702e12e
Pedro Alvarez <palvarez89@gmail.com>
2018-08-23 20:49:44 +0200

    Implement watchers

--- gitlabirced/irc_client.py
+++ gitlabirced/irc_client.py
@@ -3,6 +3,8 @@ import irc.client
 
 from multiprocessing import Process
 import re
+import requests
+import urllib
 import sys
 
 
@@ -16,6 +18,7 @@ class MyIRCClient(irc.client.SimpleIRCClient):
         self.net_name = net_name
         self.port = port
         self.watchers = watchers
+        self.last_mention = {}
 
     def on_welcome(self, connection, event):
         for ch in self.channels:
@@ -39,19 +42,52 @@ class MyIRCClient(irc.client.SimpleIRCClient):
                 continue
 
             msg = e.arguments[0].split()
-            mr_regex = r'#[0-9]+'
-            issue_regex = r'![0-9]+'
+            mr_regex = r'#([0-9]+)'
+            issue_regex = r'!([0-9]+)'
             for m in msg:
-                if re.match(mr_regex, m):
+                issue_match = re.match(mr_regex, m)
+                if issue_match:
                     print('MR')
                     print(m)
+                    self._fetch_and_say(c, 'mr', issue_match.group(1), w)
 
-                elif re.match(issue_regex, m):
+                mr_match = re.match(issue_regex, m)
+                if mr_match:
                     print('ISSUE')
                     print(m)
+                    self._fetch_and_say(c, 'issue', mr_match.group(1), w)
 
             # Only one watcher allowed per channel. Stop.
             break
+
+    def _fetch_and_say(self, c, kind, number, watcher):
+        server = watcher.get('server', 'https://gitlab.com')
+        target = watcher['channel']
+        project_encoded = urllib.parse.quote(watcher['project'], safe='')
+
+        if kind == 'issue':
+            url_template = 'api/v4/projects/{project}/issues/{number}'
+            prefix_template = 'Issue !{number}:'
+        elif kind == 'mr':
+            url_template = 'api/v4/projects/{project}/merge_requests/{number}'
+            prefix_template = 'MR #{number}:'
+
+        url = urllib.parse.urljoin(server, url_template.format(
+            project=project_encoded, number=number))
+        print(url)
+
+        data = requests.get(url)
+        info = data.json()
+        status = data.status_code
+        if status != 200:
+            return
+
+        print('Title', info['title'])
+        print('URL', info['web_url'])
+        prefix = prefix_template.format(number=number)
+        info_text = '{prefix} {title} {url}'.format(
+                prefix=prefix, title=info['title'], url=info['web_url'])
+        c.privmsg(target, info_text)
 
 
 def connect_networks(networks, watchers):
