feff88eb775af0e7ad828387bc8b32f91bf2bdd5
Pedro Alvarez <palvarez89@gmail.com>
2018-08-27 01:24:54 +0200

    test_irc_client: Add more tests
    New tests cover:
    - MR and issue mention detection
    - Spam detection
    - channel connections

--- tests/test_irc_client.py
+++ tests/test_irc_client.py
@@ -1,4 +1,5 @@
 import unittest
+from irc.client import Event
 
 from gitlabirced.irc_client import MyIRCClient
 
@@ -6,11 +7,15 @@ from gitlabirced.irc_client import MyIRCClient
 class FakeConnection():
     def __init__(self):
         self.privmsgs = {}
+        self.channels = []
 
     def privmsg(self, target, msg):
         msgs = self.privmsgs.get(target, [])
         msgs.append("(%s) %s" % (target, msg))
         self.privmsgs[target] = msgs
+
+    def join(self, channel):
+        self.channels.append(channel)
 
 
 class BaseIRCClientTestCase(unittest.TestCase):
@@ -20,22 +25,83 @@ class BaseIRCClientTestCase(unittest.TestCase):
 
     def setUp(self):
         self.connection = FakeConnection()
-        self.mycli = MyIRCClient([], 'nick', 'freenode.org', 'freenode')
+        self.target = '#target'
+
+        watcher = {'network': 'freenode',
+                   'channel': self.target,
+                   'project': 'namespace/project'}
+        self.mycli = MyIRCClient(
+            [self.target], 'nick', 'freenode.org', 'freenode',
+            watchers=[watcher], nickpass="p4ssw0rd")
+
         self.mycli._fetch_gitlab_info = self._fake_info
 
-    def test_fetch_and_say(self):
+    def test_on_pubmsg_irrelevant(self):
+        event = Event('pubmsg', '@somebody', self.target, ['a message'])
+
+        # Receive a pubmsg
+        self.mycli.on_pubmsg(self.connection, event)
+        self.assertEqual(self.mycli.count_per_channel[self.target], 1)
+
+        # Receive a second pubmsg
+        self.mycli.on_pubmsg(self.connection, event)
+        self.assertEqual(self.mycli.count_per_channel[self.target], 2)
+
+        self.assertEqual(self.connection.privmsgs, {})
+
+    def test_on_pubmsg_issue(self):
+        self.title = "Title of the issue"
+        self.code = 200
+        expected_msg = ("(#target) Issue !12: Title of the issue https://"
+                        "gitlab.com/api/v4/projects/namespace%2Fproject/"
+                        "issues/12")
+
+        self._test_on_pubmsg_generic('show me !12 plz', expected_msg)
+
+    def test_on_pubmsg_mr(self):
         self.title = "Title of the mr"
         self.code = 200
-        target = '#target'
+        expected_msg = ("(#target) MR #12: Title of the mr https://"
+                        "gitlab.com/api/v4/projects/namespace%2Fproject/"
+                        "merge_requests/12")
 
-        watcher = {'channel': target, 'project': 'namespace/project'}
-        self.mycli._fetch_and_say(self.connection, 'mr', '12', watcher)
+        self._test_on_pubmsg_generic('show me #12 plz', expected_msg)
 
-        self.assertEqual(len(self.connection.privmsgs[target]), 1)
+    def _test_on_pubmsg_generic(self, msg, expected):
+        event = Event('pubmsg', '@somebody', self.target, [msg])
+
+        # Receive a message with an issue number tag
+        self.mycli.on_pubmsg(self.connection, event)
+        # Expect an answer from the bot
+        self.assertEqual(self.mycli.count_per_channel[self.target], 1)
+        self.assertEqual(len(self.connection.privmsgs[self.target]), 1)
         self.assertEqual(
-            self.connection.privmsgs[target][-1],
-            "(#target) MR #12: Title of the mr https://gitlab.com/api"
-            "/v4/projects/namespace%2Fproject/merge_requests/12")
+            self.connection.privmsgs[self.target][-1], expected)
 
-        self.mycli._fetch_and_say(self.connection, 'mr', '12', watcher)
-        self.assertEqual(len(self.connection.privmsgs[target]), 1)
+        # Receive 'spam_threshold' more messages
+        for i in range(self.mycli.spam_threshold):
+            self.mycli.on_pubmsg(self.connection, event)
+            self.assertEqual(self.mycli.count_per_channel[self.target], 2 + i)
+        # Expect the bot to stay quiet
+        self.assertEqual(len(self.connection.privmsgs[self.target]), 1)
+
+        # Receive the same message once more
+        self.mycli.on_pubmsg(self.connection, event)
+        # Expect the bot to reply again
+        self.assertEqual(self.mycli.count_per_channel[self.target], 17)
+        self.assertEqual(len(self.connection.privmsgs[self.target]), 2)
+        self.assertEqual(
+            self.connection.privmsgs[self.target][-1], expected)
+
+    def test_on_welcome(self):
+        event = Event('welcome', 'freenode.net', 'me', ['WELCOME!'])
+
+        # Expect an IDENTIFY message to NickServ
+        self.mycli.on_welcome(self.connection, event)
+        self.assertEqual(
+            self.connection.privmsgs['NickServ'][-1],
+            "(NickServ) IDENTIFY p4ssw0rd")
+        # Expect some channels joined
+        self.assertEqual(
+            self.connection.channels,
+            [self.target])
