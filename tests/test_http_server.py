5f063d733a61db082a5381a562a73f535f5912d9
Pedro Alvarez <palvarez89@gmail.com>
2018-08-27 03:04:02 +0200

    test_http_server: Avoid deadlock on test exception
    The root cause of this deadlock was `self.con.connect()`
    happening in the setUp and being left open. The server
    would never shutdown with a connection open.

--- tests/test_http_server.py
+++ tests/test_http_server.py
@@ -1,4 +1,3 @@
-import http.client
 import urllib.parse
 import json
 
@@ -7,14 +6,8 @@ from helpers_http_server import BaseServerTestCase
 
 class BaseHTTPServerTestCase(BaseServerTestCase):
 
-    def setUp(self):
-        BaseServerTestCase.setUp(self)
-        self.con = http.client.HTTPConnection(self.HOST, self.PORT)
-        self.con.connect()
-
     def test_get_disabled(self):
-        self.con.request('GET', '/')
-        res = self.con.getresponse()
+        res = self.request('/', method='GET')
         self.assertEqual(res.status, 501)
 
     def test_post_no_json(self):
@@ -22,23 +15,20 @@ class BaseHTTPServerTestCase(BaseServerTestCase):
             {'@number': 12524, '@type': 'issue', '@action': 'show'}
         )
         headers = {"X-Gitlab-Token": "12345"}
-        self.con.request("POST", "", body=params, headers=headers)
-        res = self.con.getresponse()
+        res = self.request('', method='POST', body=params, headers=headers)
         self.assertEqual(res.status, 400)
         self.assertEqual(res.reason, "JSON data couldn't be parsed")
 
     def test_post_no_token(self):
         headers = {"Content-type": "application/x-www-form-urlencoded",
                    "Accept": "text/plain"}
-        self.con.request("POST", "", headers=headers)
-        res = self.con.getresponse()
+        res = self.request('', method='POST', headers=headers)
         self.assertEqual(res.status, 400)
         self.assertEqual(res.reason, "'X-Gitlab-Token' header not found")
 
     def test_post_wrong_token(self):
         headers = {"X-Gitlab-Token": "9999"}
-        self.con.request("POST", "", headers=headers)
-        res = self.con.getresponse()
+        res = self.request('', method='POST', headers=headers)
         self.assertEqual(res.status, 401)
         self.assertEqual(res.reason, "Gitlab token not authorized")
 
@@ -46,8 +36,8 @@ class BaseHTTPServerTestCase(BaseServerTestCase):
         params = {'something_else': 'push'}
         headers = {"X-Gitlab-Token": "12345"}
         params_json = json.dumps(params)
-        self.con.request("POST", "", body=params_json, headers=headers)
-        res = self.con.getresponse()
+        res = self.request('', method='POST', body=params_json,
+                           headers=headers)
         self.assertEqual(res.status, 400)
         self.assertEqual(res.reason, "Missing 'object_kind'")
 
@@ -55,7 +45,7 @@ class BaseHTTPServerTestCase(BaseServerTestCase):
         params = {'object_kind': 'foo'}
         headers = {"X-Gitlab-Token": "12345"}
         params_json = json.dumps(params)
-        self.con.request("POST", "", body=params_json, headers=headers)
-        res = self.con.getresponse()
+        res = self.request('', method='POST', body=params_json,
+                           headers=headers)
         self.assertEqual(res.status, 400)
         self.assertEqual(res.reason, "object_kind 'foo' not supported")
