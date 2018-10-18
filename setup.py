7f303def9f74aaa6d3250d0626c3d1893be0c819
Pedro Alvarez <palvarez89@gmail.com>
2018-10-18 21:47:05 +0100

    setup.py: Remove unsupported python versions

--- setup.py
+++ setup.py
@@ -21,12 +21,10 @@ setup(
     author="Pedro Alvarez Piedehierro",
     author_email='pedro@alvarezpiedehierro.com',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
